4b2f20eb152e2a0b31b278f2dd389e309c589d24
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-19 14:32:34 +0300

    Update user.py

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,10 +2,17 @@ from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
 
 from loader import dp
-from tgbot.keyboards.reply.location import create_markup
+from tgbot.keyboards.reply.location import create_markup as location_markup
+from tgbot.keyboards.inline.places_keyboard import create_markup as places_markup
 
 
 @dp.message_handler(Command('rs'))
 async def find_restaurants(message: Message) -> Message:
-    markup = create_markup()
+    markup = location_markup()
     return await message.answer('Send location by button or just as input', reply_markup=markup)
+
+
+@dp.message_handler(Command('places'))
+async def show_places(message: Message) -> Message:
+    markup = places_markup()
+    return await message.answer('Оберіть вільне місце, яке хочете забронювати', reply_markup=markup)
