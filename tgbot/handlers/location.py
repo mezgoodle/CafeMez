5ec44012703a8e63aca01703a9b2b57e1f557f28
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-19 14:07:50 +0300

    Update location.py

--- tgbot/handlers/location.py
+++ tgbot/handlers/location.py
@@ -1,27 +1,27 @@
 from aiogram.types import Message, ContentType
+from aiogram.utils.markdown import hlink
 
 from tgbot.misc.calc_distance import choose_shortest
 from loader import dp
 
 
 @dp.message_handler(content_types=ContentType.LOCATION)
-async def nearest_restaurants(message: Message) -> None:
+async def nearest_restaurants(message: Message) -> Message:
     location = message.location
     latitude = location.latitude
     longitude = location.longitude
     closest_restaurants = choose_shortest(location)
-    print(closest_restaurants[0][2])
-    text_format = "Назва: {name}. <a href='{url}'>Google</a>\n Відстань до нього: {distance:.1f} км."
+    text_format = "Назва: {name}. {url}\nВідстань до нього: {distance:.1f} км."
     text = '\n\n'.join(
         [
-            text_format.format(name=name, url=url, distance=distance)
+            text_format.format(name=name, url=hlink('Google', url), distance=distance)
             for name, distance, url, location in closest_restaurants
         ]
     )
-    print(text)
     await message.answer(f'Дякую!\n'
                          f'Ваші координати: {latitude}, {longitude}\n\n'
                          f'{text}',
                          disable_web_page_preview=True)
     for name, distance, url, location in closest_restaurants:
         await message.answer_location(location['lat'], location['lot'])
+    return await message.answer('Скористатйтеся посилання на Google карти')
