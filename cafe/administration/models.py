a7fd9504e2dc02c6a7b09dfead627c3ceb559d3a
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-20 17:46:38 +0300

    Update models.py
    Create User model

--- cafe/administration/models.py
+++ cafe/administration/models.py
@@ -23,3 +23,18 @@ class Place(TimeStampedModel):
 
     def __str__(self):
         return f'Місце {self.id} - {self.free}'
+
+
+class User(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    user_id = models.BigIntegerField(unique=True, default=1, verbose_name='Ідентифікатор користувача у телеграмі')
+    name = models.CharField(max_length=100, verbose_name='Ім\'я користувача')
+    username = models.CharField(max_length=100, verbose_name='Ім\'я користувача в телеграмі')
+    email = models.EmailField(max_length=100, verbose_name='Електронна пошта')
+
+    class Meta:
+        verbose_name = 'Користувач'
+        verbose_name_plural = 'Користувачі'
+
+    def __str__(self):
+        return f'#{self.id} ({self.user_id} {self.name})'
