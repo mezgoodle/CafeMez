9f240a695ddf914d35184658bb22421c903f2daa
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-20 17:42:58 +0300

    Create model for Place

--- cafe/administration/models.py
+++ cafe/administration/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Place(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    free = models.BooleanField(default=True, verbose_name='Вільне місце')
+
+    class Meta:
+        verbose_name = 'Місце'
+        verbose_name_plural = 'Місця'
+
+    def __str__(self):
+        return f'Місце {self.id} - {self.free}'
