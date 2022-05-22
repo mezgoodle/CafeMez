6f83ea45822b2ed84c53919bee8aa2d5b9f0fa11
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-22 14:05:12 +0300

    Update user.py

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -9,7 +9,7 @@ from tgbot.keyboards.inline.places_keyboard import places_markup
 @dp.message_handler(Command('rs'))
 async def find_restaurants(message: Message) -> Message:
     markup = location_markup()
-    return await message.answer('Send location by button or just as input', reply_markup=markup)
+    return await message.answer('Надішліть локацію через кнопку або просто як вкладення', reply_markup=markup)
 
 
 @dp.message_handler(Command('places'))
