b1feea3e41ab15c6ed2471f2a13bb886f55cc8e3
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-23 14:23:23 +0300

    Update callbacks.py
    Send invoice

--- tgbot/handlers/callbacks.py
+++ tgbot/handlers/callbacks.py
@@ -1,11 +1,13 @@
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, LabeledPrice
 from aiogram.utils.markdown import hbold
 
 from tgbot.keyboards.inline.places_keyboard import place_markup, places_markup
 from tgbot.keyboards.inline.callback_data import place_callback as place_data
-from loader import dp
+from loader import dp, bot
 
 from datetime import datetime
+
+from tgbot.misc.invoices.invoice import Item
 
 
 @dp.callback_query_handler(text='busy_place')
@@ -14,7 +16,20 @@ async def place_callback(call: CallbackQuery) -> Message:
 
 
 @dp.callback_query_handler(place_data.filter(choice='yes'))
-async def accept_offer(call: CallbackQuery) -> Message:
+async def accept_offer(call: CallbackQuery, callback_data: dict) -> Message:
+    number = callback_data['number']
+    place_invoice = Item(
+        title=f'Місце з номером {number}',
+        description=f'Оренда місця з номером {call.data.split(":")[-1]} на дату {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+        currency='UAH',
+        prices=[
+            LabeledPrice(
+                label='Оренда столика',
+                amount=10
+            )],
+        start_parameter=f'create_invoice_rent_place_{number}',
+    )
+    await bot.send_invoice(call.from_user.id, **place_invoice.generate_invoice(), payload=f'place:{number}')
     return await call.message.reply('Непогано.')
 
 
