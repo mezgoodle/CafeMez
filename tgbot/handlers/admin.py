cff399cad548cdaa3f2a109ea138d10e31b28fef
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-23 19:54:08 +0300

    Update admin.py

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from aiogram.dispatcher.filters import Command
 
 from loader import dp
@@ -7,3 +7,14 @@ from loader import dp
 @dp.message_handler(Command(['stats']), is_general_admin=True)
 async def show_stats(message: Message) -> Message:
     return await message.reply(f'Hello, {message.from_user.username}!')
+
+
+@dp.message_handler(content_types=ContentType.LOCATION, is_general_admin=True)
+async def add_cafe_coords(message: Message) -> Message:
+    return await message.reply(f'{message.location.longitude}, {message.location.latitude}')
+
+
+@dp.message_handler(content_types=ContentType.LOCATION, is_general_admin=True)
+async def add_cafe_coords(message: Message) -> Message:
+    # TODO: send request to server
+    return await message.reply(f'{message.location.longitude}, {message.location.latitude}')
