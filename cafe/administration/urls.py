316e5f852258f36de696e93c1583a60e8afdf257
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-24 20:46:36 +0300

    Create methods for detail and posting

--- cafe/administration/urls.py
+++ cafe/administration/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('places', views.places_lists, name='all_places'),
+    path('add_places', views.places_add, name='new_place'),
+    path('places/<int:pk>', views.places_detail, name='place_detail'),
 ]
--- cafe/administration/views.py
+++ cafe/administration/views.py
@@ -10,3 +10,19 @@ from .serializers import PlaceSerializer
 def places_lists(request):
     places = PlaceSerializer(Place.objects.all(), many=True)
     return Response(places.data)
+
+
+@api_view(['GET'])
+def places_detail(request, pk):
+    place = Place.objects.get(pk=pk)
+    place = PlaceSerializer(place, many=False)
+    return Response(place.data)
+
+
+@api_view(['POST'])
+def places_add(request):
+    place = PlaceSerializer(data=request.data)
+    if place.is_valid():
+        place.save()
+        return Response(place.data)
+    return Response(place.errors)
