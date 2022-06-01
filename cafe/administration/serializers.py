5625aa181ae0a8ffefcea9045d9a3bd4ee358e20
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-01 13:24:48 +0300

    Update serializers.py
    Create serializer for Restaurant

--- cafe/administration/serializers.py
+++ cafe/administration/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Place
+from .models import Place, Restaurant
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
+        fields = '__all__'
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
         fields = '__all__'
