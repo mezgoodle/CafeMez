b219e1babb855e751038078f2ec6c3c9c6ab740a
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-02 20:10:24 +0300

    Update api.py

--- tgbot/misc/api.py
+++ tgbot/misc/api.py
@@ -6,13 +6,18 @@ class API:
         self.session = aiohttp.ClientSession()
         self.base_url = 'http://127.0.0.1:8000/api/%s'
 
-    async def get(self, request: str):
-        url = self.base_url % request
+    async def get(self, request_url: str):
+        url = self.base_url % request_url
         async with self.session.get(url) as response:
             return await response.json()
 
-    async def post(self, url, data=None):
+    async def post(self, request_url, data=None):
+        url = self.base_url % request_url
         async with self.session.post(url, data=data) as response:
+            return await response.json()
+
+    async def put(self, url, data=None):
+        async with self.session.put(url, data=data) as response:
             return await response.json()
 
     async def close(self):
