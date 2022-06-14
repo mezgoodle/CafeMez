d0165860eb4ac6f142f5ed72630292d57be6002c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-14 14:14:47 +0300

    Update restaurants_keyboard.py

--- tgbot/keyboards/inline/restaurants_keyboard.py
+++ tgbot/keyboards/inline/restaurants_keyboard.py
@@ -3,13 +3,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from tgbot.keyboards.inline.callback_data import place_callback
 
 
-async def restaurants_markup(message: Message) -> InlineKeyboardMarkup:
-    api = message.bot.get('api')
-    restaurants = await api.get('restaurants')
-    markup = InlineKeyboardMarkup(row_width=2)
+async def restaurants_markup(restaurants: list) -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup(row_width=4)
     for restaurant in restaurants:
-        restaurant_button = InlineKeyboardButton(text=restaurant['name'], callback_data='none')
-        cancel_button = InlineKeyboardButton(text="❌", callback_data='delete_rs')
-        markup.add(restaurant_button, cancel_button)
-        # markup.insert(restaurant_button)
+        cancel_button = InlineKeyboardButton(text=f"{restaurant['id']}❌", callback_data='delete_rs')
+        markup.insert(cancel_button)
     return markup
