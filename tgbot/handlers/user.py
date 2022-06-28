a6dcfbed30f2be9625a867c85d58787cd1606316
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-28 16:08:04 +0300

    Update user.py
    Add one more handler

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,8 +1,10 @@
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
+from aiogram.dispatcher import FSMContext
 
 from loader import dp
 from tgbot.keyboards.reply.location import location_markup
+from tgbot.keyboards.reply.restaurants import restaurants_markup
 from tgbot.keyboards.inline.places_keyboard import places_markup
 
 
@@ -13,7 +15,16 @@ async def find_restaurants(message: Message) -> Message:
 
 
 @dp.message_handler(Command(['places', 'admin_places']))
-async def show_places(message: Message) -> Message:
-    markup = await places_markup(message)
+async def show_restaurants(message: Message, state: FSMContext) -> Message:
+    markup = await restaurants_markup(message)
+    await state.set_state('restaurant_name')
+    return await message.answer('Оберіть ресторан, у якому хочете забронювати місце', reply_markup=markup)
+
+
+@dp.message_handler(state='restaurant_name')
+async def show_places(message: Message, state: FSMContext) -> Message:
+    restaurant_name = message.text
+    await state.finish()
+    markup = await places_markup(message, restaurant_name)
     await message.answer('Оберіть вільне місце, яке хочете забронювати', reply_markup=markup)
     return await message.reply('Оренда буде дійсна дві години від початку оренди')
