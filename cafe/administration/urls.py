67f07fe65a519b62c4c3ebaafff2a44a9e6f4c31
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-28 09:30:08 +0300

    Update urls.py
    Create url patterns for User

--- cafe/administration/urls.py
+++ cafe/administration/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('places/<int:pk>', views.PlaceDetail.as_view(), name='place_detail'),
     path('restaurants', views.RestaurantList.as_view(), name='all_restaurants'),
     path('restaurants/<int:pk>', views.RestaurantDetail.as_view(), name='restaurant_detail'),
+    path('users', views.UserList.as_view(), name='all_users'),
+    path('users/<str:username>', views.UserDetail.as_view(), name='user_detail'),
     path('token', obtain_auth_token, name='token'),
 ]
