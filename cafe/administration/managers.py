96cf1430bdf4d4a490f9d9f81f4f427229528e75
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-29 11:35:48 +0300

    Try to add admin

--- cafe/administration/managers.py
+++ cafe/administration/managers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import gettext_lazy as _
+
+from .models import *
 
 
 class CustomUserManager(BaseUserManager):
@@ -15,9 +19,29 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
+        # TODO: dont get password from user, if it is not staff
+        # print(password)
         user.set_password(password)
+        # TODO: fix this
+        if extra_fields.get('is_staff'):
+            # print('Hello')
+            needed_models = [Place, Restaurant, Referral, Purchase, Item]
+            for model in needed_models:
+                content_type = ContentType.objects.get_for_model(model)
+                permissions = Permission.objects.filter(content_type=content_type)
+                for permission in permissions:
+                    user.user_permissions.add(permission)
         user.save()
         return user
+
+    def create_staffuser(self, email, password, **extra_fields):
+        """
+        Create and save a StaffUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         """
