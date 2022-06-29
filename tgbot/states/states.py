825fcfe0468955f225e01b0aa294eef8443bf076
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-06-29 11:34:55 +0300

    Update states.py
    Create state for Admin

--- tgbot/states/states.py
+++ tgbot/states/states.py
@@ -1,5 +1,8 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-class Example(StatesGroup):
-    example = State()
+class Admin(StatesGroup):
+    username = State()
+    password = State()
+    email = State()
+    is_staff = State()
