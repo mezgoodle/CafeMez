037cf7b5e77234bf16f4892f61d620ca29f783f7
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-02 18:55:54 +0300

    Update start.py
    Change the regular expression

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -7,10 +7,10 @@ from loader import dp
 from re import compile
 
 
-@dp.message_handler(CommandStart(deep_link=compile(r'^[0-9]{3,15}$')), ChatTypeFilter(ChatType.PRIVATE))
-async def register_refferal(message: Message):
-    refferal = message.get_args()
-    await message.answer(refferal)
+@dp.message_handler(CommandStart(deep_link=compile(r'^[a-z0-9_-]{3,15}$')), ChatTypeFilter(ChatType.PRIVATE))
+async def register_referral(message: Message):
+    referral = message.get_args()
+    await message.answer(referral)
 
 
 @dp.message_handler(CommandStart())
