cab70de0f77ef221f463372bd6728be55b93e4ab
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-03 09:47:34 +0300

    Add loguru to django side

--- cafe/administration/views.py
+++ cafe/administration/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, views, response, status
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.http import Http404
+from loguru import logger
 
 from .models import Place, Restaurant, User, Referral
 from .serializers import PlaceSerializer, RestaurantSerializer, UserSerializer, ReferralSerializer
@@ -11,9 +12,33 @@ from .utils import set_permissions
 class ListView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def get(self, request, *args, **kwargs):
+        logger.info(f'Get list of objects; {request=}; {args=}; {kwargs=}')
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logger.info(f'Create object; {request=}; {args=}; {kwargs=}')
+        return self.create(request, *args, **kwargs)
+
 
 class DetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        logger.info(f'Get object; {request=}; {args=}; {kwargs=}')
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        logger.info(f'Update object; {request=}; {args=}; {kwargs=}')
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        logger.info(f'Update object; {request=}; {args=}; {kwargs=}')
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        logger.info(f'Delete object; {request=}; {args=}; {kwargs=}')
+        return self.destroy(request, *args, **kwargs)
 
 
 class PlaceList(ListView):
@@ -35,8 +60,10 @@ def places_by_restaurant(request, restaurant_name):
     try:
         restaurant = Restaurant.objects.get(name=restaurant_name)
     except Restaurant.DoesNotExist:
+        logger.error(f'Restaurant {restaurant_name} does not exist')
         raise Http404
 
+    logger.info(f'Get places by restaurant; {request=}; {restaurant_name=}')
     places = Place.objects.filter(restaurant=restaurant)
     serializer = PlaceSerializer(places, many=True)
     return response.Response(serializer.data)
@@ -76,11 +103,13 @@ class UserList(views.APIView):
     """
 
     def get(self, request):
+        logger.info(f'Get list of users; {request=}')
         snippets = User.objects.all()
         serializer = UserSerializer(snippets, many=True)
         return response.Response(serializer.data)
 
     def post(self, request):
+        logger.info(f'Create user; {request=}; {request.data=}')
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -101,14 +130,17 @@ class UserDetail(views.APIView):
         try:
             return User.objects.get(username=username)
         except User.DoesNotExist:
+            logger.error(f'User {username} does not exist')
             raise Http404
 
     def get(self, request, username):
+        logger.info(f'Get user; {request=}; {username=}')
         user = self.get_object(username)
         serializer = UserSerializer(user)
         return response.Response(serializer.data)
 
     def put(self, request, username):
+        logger.info(f'Update user; {request=}; {username=}; {request.data=}')
         user = self.get_object(username)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
@@ -117,6 +149,7 @@ class UserDetail(views.APIView):
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, username):
+        logger.info(f'Delete user; {request=}; {username=}')
         user = self.get_object(username)
         user.delete()
         return response.Response(status=status.HTTP_204_NO_CONTENT)
--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from loguru import logger
+
+logger.add('django.log', format='{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}', rotation='5 KB',
+           compression='zip', enqueue=True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +30,6 @@ SECRET_KEY = 'django-insecure-ntm_kl54r#=fqz%p*60+-(pt!%x^r=)-%7d&ypvjx*gbu1lixy
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cafe.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -90,7 +92,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -110,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -121,7 +121,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
