6878ed9160e8327cf9d790bcbe38810fd48824e0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-03 21:28:35 +0300

    Update callback_data.py
    Create callback data for support

--- tgbot/keyboards/inline/callback_data.py
+++ tgbot/keyboards/inline/callback_data.py
@@ -2,3 +2,5 @@ from aiogram.utils.callback_data import CallbackData
 
 place_callback = CallbackData('offer', 'number', 'choice')
 rs_callback = CallbackData('rs', 'number')
+support_callback = CallbackData('ask_support', 'messages', 'user_id', 'as_user')
+cancel_support = CallbackData('cancel_support', 'user_id')
