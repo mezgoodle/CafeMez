23e80f14e899a63827e3aad0b04ad43b3f9be085
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-08 10:50:27 +0300

    Create middleware for getting subcategories

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from tgbot.config import load_config
 from tgbot.filters.admin import IsGeneralAdminFilter, IsAdminFilter
 from tgbot.middlewares.throttling import ThrottlingMiddleware
 from tgbot.middlewares.callbacks import CallbackMiddleware
+from tgbot.middlewares.subcategories import SubCategoriesMiddleware
 # from tgbot.middlewares.language import ACLMiddleware
 from tgbot.middlewares.support_middleware import SupportMiddleware
 from tgbot.services.setting_commands import set_default_commands
@@ -24,6 +25,7 @@ def register_all_middlewares(dispatcher: Dispatcher) -> None:
     dispatcher.setup_middleware(ThrottlingMiddleware())
     dispatcher.setup_middleware(CallbackMiddleware())
     dispatcher.setup_middleware(SupportMiddleware())
+    dispatcher.setup_middleware(SubCategoriesMiddleware())
     # i18n = ACLMiddleware(config.i18n.domain, config.i18n.locales_dir)
     # dispatcher.setup_middleware(i18n)
     # logger.info('Add i18n middleware to the bot')
---
+++ tgbot/middlewares/subcategories.py
@@ -0,0 +1,17 @@
+from aiogram.types import Message
+from aiogram.dispatcher.middlewares import BaseMiddleware
+
+from tgbot.misc.backend import Item
+
+
+class SubCategoriesMiddleware(BaseMiddleware):
+    async def on_pre_process_message(self, message: Message, data: dict):
+        try:
+            _ = data['subcategories']
+        except KeyError:
+            api: Item = message.bot.get('items_api')
+            categories = await api.get_categories()
+            subcategories = []
+            for category in categories:
+                subcategories.extend(await api.get_subcategories(category['code']))
+            data['subcategories'] = [elem['code'] for elem in subcategories]
