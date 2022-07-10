56f5a5afe9fe6fdb6b8a1e9d51d5f1b67ddb7fce
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-10 12:17:10 +0300

    Update utils.py
    Add permissions setting for the two more models

--- cafe/administration/utils.py
+++ cafe/administration/utils.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
-from .models import Place, Restaurant, User, Referral, Purchase, Item
+from .models import Place, Restaurant, User, Referral, Purchase, Item, SubCategory, Category
 
 
 def set_permissions(user: User):
     if user.is_staff:
-        needed_models = [Place, Restaurant, Referral, Purchase, Item]
+        needed_models = [Place, Restaurant, Referral, Purchase, Item, Category, SubCategory]
         for model in needed_models:
             content_type = ContentType.objects.get_for_model(model)
             permissions = Permission.objects.filter(content_type=content_type)
