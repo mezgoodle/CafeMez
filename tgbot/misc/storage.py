097495306e67f6f36d87d8a2676f38a479890f78
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-21 20:24:28 +0300

    Create storage.py
    First try to create json storage

---
+++ tgbot/misc/storage.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+
+class Storage:
+    def __init__(self, filename: str = 'storage.json'):
+        self.filename = filename
+        self.create_file()
+
+    def create_file(self):
+        with open(self.filename, 'w') as f:
+            json.dump({}, f)
+
+    def delete_file(self):
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
+
+    def read_file(self, mode: str = 'r') -> dict:
+        with open(self.filename, mode) as f:
+            return json.load(f)
+
+    def get_cart(self, identifier: str) -> list:
+        data = self.read_file()
+        try:
+            return data[identifier]
+        except KeyError:
+            return []
+
+    def add_to_cart(self, identifier: str, item: str, quantity: int = 1) -> None:
+        data = self.read_file()
+        try:
+            cart = data[identifier]
+        except KeyError:
+            cart = []
+        cart.extend([item] * quantity)
+        data[identifier] = cart
+        with open(self.filename, 'w') as f:
+            json.dump(data, f)
+
+
+s = Storage()
+s.add_to_cart('3123123', 'test')
+print(s.get_cart('3123123'))
+s.add_to_cart('3123123', 'test1', 2)
+print(s.get_cart('3123123'))
+s.add_to_cart('3123123', 'test2')
+print(s.get_cart('3123123'))
+s.add_to_cart('31233', 'test2', 4)
+print(s.get_cart('31233'))
+print(s.get_cart('1'))
