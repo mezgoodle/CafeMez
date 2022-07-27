c8a2406bcf3c3fac984359cfdec092c96cdc8cbb
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-27 19:05:40 +0300

    Add some handlers in purchase

--- tgbot/handlers/purchase.py
+++ tgbot/handlers/purchase.py
@@ -1,13 +1,14 @@
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Command
 from aiogram.utils.markdown import hbold
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, ContentType
 
 from tgbot.handlers.menu import list_categories
 from loader import dp
 from tgbot.keyboards.inline.cart_keyboard import cart_keyboard
 from tgbot.keyboards.inline.callback_data import cart_callback
-from tgbot.misc.backend import Item
+from tgbot.keyboards.reply.restaurants import restaurants_markup
+from tgbot.keyboards.reply.payment import payments_markup
+from tgbot.misc.backend import Item, Restaurant
 from tgbot.misc.storage import Storage
 
 
@@ -73,11 +74,6 @@ async def delete_item(callback_query: CallbackQuery, callback_data: dict):
         return await callback_query.message.answer('Ваша оновлена корзина:', reply_markup=keyboard)
 
 
-@dp.callback_query_handler(text_contains='buy:')
-async def apply_purchase(callback_query: CallbackQuery, callback_data: dict):
-    pass
-
-
 @dp.callback_query_handler(text_contains='cancel_cart')
 async def cancel_purchase(callback_query: CallbackQuery, *args, **kwargs):
     storage: Storage = callback_query.bot.get('storage')
@@ -86,3 +82,38 @@ async def cancel_purchase(callback_query: CallbackQuery, *args, **kwargs):
     return await list_categories(callback_query.message)
 
 
+@dp.callback_query_handler(text_contains='buy:')
+async def apply_purchase(callback_query: CallbackQuery, state: FSMContext, *args, **kwargs):
+    await state.set_state('shipping_address')
+    keyboard = await restaurants_markup(callback_query.message)
+    text = 'Чи бажаєте ви доставку замовлення?' \
+           f'\nЯкщо {hbold("так")}, надішліть координати доставки.' \
+           f'\nЯкщо {hbold("ні")}, виберіть один із ресторанів'
+    return await callback_query.message.answer(text, reply_markup=keyboard)
+
+
+@dp.message_handler(state='shipping_address', content_types=ContentType.TEXT)
+async def shipping_address_as_text(message: Message, state: FSMContext):
+    restaurant_name = message.text
+    api: Restaurant = message.bot.get('restaurants_api')
+    restaurant = await api.get_restaurant(restaurant_name)
+    if name := restaurant['name']:
+        await state.update_data(restaurant_name=name)
+        await state.set_state('payment_method')
+        keyboard = payments_markup()
+        return await message.answer('Виберіть спосіб оплати', reply_markup=keyboard)
+
+
+@dp.message_handler(state='shipping_address', content_types=ContentType.LOCATION)
+async def shipping_address_as_location(message: Message, state: FSMContext):
+    restaurant_location = message.location
+    await state.update_data(longitude=restaurant_location.longitude, latitude=restaurant_location.latitude)
+    await state.set_state('payment_method')
+    keyboard = payments_markup()
+    return await message.answer('Виберіть спосіб оплати', reply_markup=keyboard)
+
+
+@dp.message_handler(lambda message: message.text in ['Картка', 'Готівка'], state='payment_method')
+async def answer_payment_method(message: Message, state: FSMContext):
+    await state.update_data(payment_method=message.text)
+    return await message.answer('Ви обрали спосіб оплати: ' + message.text)
---
+++ tgbot/keyboards/reply/payment.py
@@ -0,0 +1,7 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+
+def payments_markup() -> ReplyKeyboardMarkup:
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup.row(KeyboardButton(text='Готівка'), KeyboardButton(text='Картка'))
+    return markup
