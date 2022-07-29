b68b06ef4f348c43a15374071e8ad689419d9265
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-29 19:54:05 +0300

    Update location.py

--- tgbot/keyboards/reply/location.py
+++ tgbot/keyboards/reply/location.py
@@ -3,5 +3,5 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 def location_markup() -> ReplyKeyboardMarkup:
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    markup.add(KeyboardButton('Send your location', request_location=True))
+    markup.add(KeyboardButton('Надіслати локацію', request_location=True))
     return markup
