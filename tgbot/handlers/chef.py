71a60ec16e27478cc71dee1e346e5a9ab2de502c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-31 19:35:18 +0300

    Show orders to the chef

--- tgbot/handlers/chef.py
+++ tgbot/handlers/chef.py
@@ -1,12 +1,21 @@
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 
 from loader import dp
 from tgbot.misc.backend import User
+from tgbot.keyboards.inline.orders import orders_keyboard
 
 
 @dp.message_handler(Command("my_orders"), is_chef=True)
 async def show_orders(message: Message):
     api: User = message.bot.get('users_api')
     orders = await api.get_orders(message.from_user.username)
-    return await message.answer(f'Ваші замовлення:\n{orders}')
+    for order in orders:
+        text = f'{hbold("Замовлення номер - ") + hbold(order["id"])}\n'
+        keyboard = orders_keyboard(order)
+        await message.answer(text, reply_markup=keyboard)
+    return await message.answer(f'Щоб побачити деталі продукту - {hbold("натисніть на продукт")}\n'
+                                f'Щоб змінити статус замовлення - {hbold("натисніть на кнопку Готове або Не готове")}\n'
+                                f'Щоб змінити статус оплати - {hbold("натисніть на кнопку Оплачено або Не оплачено")}\n'
+                                f'Щоб видалити замовлення - {hbold("натисніть на кнопку Видалити")}')
---
+++ tgbot/keyboards/inline/orders.py
@@ -0,0 +1,22 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def orders_keyboard(order):
+    keyboard = InlineKeyboardMarkup(row_width=4)
+    buttons = []
+    for item in order['items']:
+        buttons.append(InlineKeyboardButton(text=f'{item["item"]["name"]} - {item["quantity"]}', callback_data=f'text'))
+    keyboard.row(*buttons)
+    keyboard.row(
+        InlineKeyboardButton(text=f'{"Оплачене" if order["is_paid"] else "Не оплачене"}', callback_data=f'text'),
+        InlineKeyboardButton(text=f'{"Готове" if order["is_paid"] else "Не готове"}', callback_data=f'text'),
+    )
+    keyboard.add(InlineKeyboardButton(text=f'Сума: {order["total_price"]}', callback_data=f'text'))
+    keyboard.row(
+        InlineKeyboardButton(text=f'Замовник - {order["user"]}', url=f'https://t.me/{order["user"]}'),
+        InlineKeyboardButton(
+            text=f'Кур\'єр - {order["connected_courier"] if order["connected_courier"] else "немає"}',
+            url=f'https://t.me/{order["connected_courier"]}'),
+    )
+    keyboard.add(InlineKeyboardButton(text=f'Видалити замовлення', callback_data=f'text'))
+    return keyboard
