3e3d4e02be2edf1f1d2885ff0f80258c3751b617
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-31 21:02:55 +0300

    Update serializers.py

--- cafe/administration/serializers.py
+++ cafe/administration/serializers.py
@@ -47,7 +47,6 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
-        depth = 1
 
 
 class PlaceSerializer(serializers.ModelSerializer):
