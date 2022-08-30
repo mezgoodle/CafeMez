f4d2a43065fc36d8f7d5856a2b33a712b369a96d
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-08-30 17:51:09 +0300

    Update payments.py
    Apply handling functions

--- tgbot/handlers/payments.py
+++ tgbot/handlers/payments.py
@@ -1,10 +1,19 @@
-from aiogram.types import PreCheckoutQuery
+from aiogram.types import PreCheckoutQuery, ContentType, Message
 
-from loader import dp, bot
+from loader import dp
+from tgbot.misc.payments import handle_place_payment, handle_order_payment
 
 
 @dp.pre_checkout_query_handler()
 async def pre_checkout(query: PreCheckoutQuery):
-    print(query.order_info)
-    await bot.answer_pre_checkout_query(query.id, ok=True)
-    await bot.send_message(query.from_user.id, "Дякуємо за оренду!")
+    payload_data = query.invoice_payload.split(':')
+    payment_handlers = {
+        'place': handle_place_payment,
+        'order': handle_order_payment,
+    }
+    return await payment_handlers[payload_data[0]](query, payload_data, query.from_user.username)
+
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def process_pay(message: Message):
+    return await message.answer('Оплата теж успішна! Не видаляйте чек. Вам необхідно показати його у ресторані!')
