f8d111e42bca8c1e000c0ea1638b2df33a2276d0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-08-31 09:35:56 +0300

    Update payments.py
    Notify staff about reservation a table

--- tgbot/misc/payments.py
+++ tgbot/misc/payments.py
@@ -1,15 +1,26 @@
 from aiogram import Bot
 from aiogram.types import PreCheckoutQuery, Message
+from aiogram.utils.markdown import hbold
 
-from tgbot.misc.backend import Place, Order
+from tgbot.misc.backend import Place, Order, User
+
+from random import choice
 
 
 async def handle_place_payment(query: PreCheckoutQuery, data: list, username: str) -> Message:
     bot: Bot = query.bot
-    api: Place = bot.get('places_api')
-    _, status = await api.update_place(data[1], {'free': False, 'customer': username})
+    users_api: User = bot.get('users_api')
+    places_api: Place = bot.get('places_api')
+    place_id = data[1]
+    rs_name = data[2]
+    _, status = await places_api.update_place(place_id, {'free': False, 'customer': username})
     if status == 200:
         await bot.answer_pre_checkout_query(query.id, ok=True)
+        staff = await users_api.get_staff(rs_name)
+        staff = staff['staff']
+        admin = choice(staff)
+        await bot.send_message(admin['telegram_id'],
+                               f'Столик із номером {hbold(place_id)} у ресторані {hbold(rs_name)} зарезервований')
         return await bot.send_message(query.from_user.id, "Оренда успішна! Працюємо із грошима💰")
     else:
         await bot.answer_pre_checkout_query(query.id, ok=False)
