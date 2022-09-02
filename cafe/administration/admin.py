c83cab499e829424c4a680613c5dbbf418357d57
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-09-02 09:44:31 +0300

    Update admin.py

--- cafe/administration/admin.py
+++ cafe/administration/admin.py
@@ -5,8 +5,8 @@ from .models import Item, User, Place, Referral, Restaurant, Category, SubCatego
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_staff', 'is_active', 'telegram_id')
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'is_staff', 'is_chef', 'is_courier', 'is_active', 'telegram_id')
+    list_filter = ('email', 'is_staff', 'is_chef', 'is_courier', 'is_active',)
     search_fields = ('email', 'telegram_id', 'username')
     ordering = ('email',)
 
