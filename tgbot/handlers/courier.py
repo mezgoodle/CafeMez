7c3e11e777832499e6382cf7bc8299fe9f88e2bc
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-09-02 09:01:15 +0300

    Update chef and courier logic
    Move payment changing from chef to courier. Notify, when order is delievered

--- tgbot/handlers/courier.py
+++ tgbot/handlers/courier.py
@@ -25,9 +25,27 @@ async def show_courier_order(message: Message):
     return await message.answer('На даний момент немає замовлень')
 
 
+@dp.callback_query_handler(order_callback.filter(action='paid'), is_admin=True)
+@dp.callback_query_handler(order_callback.filter(action='paid'), is_courier=True)
+async def change_order_payment(callback_query: CallbackQuery, callback_data: dict):
+    api: Order = callback_query.bot.get('orders_api')
+    order = await api.get_order(callback_data['id'])
+    if order['connected_courier'] == callback_query.from_user.username:
+        new_order, status = await api.update_order(
+            callback_data['id'],
+            {'is_paid': callback_data['value']}
+        )
+        if status == 200:
+            keyboard = orders_keyboard(new_order)
+            await callback_query.message.answer('Статус оплати змінено!')
+            return await callback_query.message.edit_reply_markup(reply_markup=keyboard)
+        return await callback_query.message.answer('Помилка при зміні статусу оплати!')
+
+
 @dp.callback_query_handler(order_callback.filter(action='delivered'), is_courier=True)
 async def change_order_delivered(callback_query: CallbackQuery, callback_data: dict):
     api: Order = callback_query.bot.get('orders_api')
+    bot: Bot = callback_query.bot
     order = await api.get_order(callback_data['id'])
     if order['connected_courier'] == callback_query.from_user.username:
         new_order, status = await api.update_order(
@@ -36,6 +54,7 @@ async def change_order_delivered(callback_query: CallbackQuery, callback_data: d
         )
         if status == 200:
             keyboard = orders_keyboard(new_order)
+            await bot.send_message(order['user']['telegram_id'], 'Ваше замовлення доставлене')
             await callback_query.message.answer('Статус доставки змінено!')
             return await callback_query.message.edit_reply_markup(reply_markup=keyboard)
         return await callback_query.message.answer('Помилка при зміні статусу доставки!')
