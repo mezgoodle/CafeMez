7ab07d2185d73ad2a85e1b6fddd638d703fedacb
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-09-04 09:54:07 +0300

    Update help.py
    Make better text

--- tgbot/handlers/help.py
+++ tgbot/handlers/help.py
@@ -4,23 +4,26 @@ from aiogram.dispatcher.filters import CommandHelp, Command
 from loader import dp
 
 
-# TODO: make better text
 @dp.message_handler(Command('courier_help'), state='*', is_courier=True)
 async def courier_help_command(message: Message) -> Message:
-    return await message.reply('Вітаємо! Щоб почати користуватись ботом, ви можете:\n'
+    return await message.reply('Вітаємо! Ви повинні брати замовлення зі списку та змінювати статус доставки.\n\n'
+                               'Щоб почати користуватись ботом, ви можете:\n'
                                '* /orders - показати усі замовлення вашого ресторану\n'
                                '* /my_order - показати замовлення, що ви узяли')
 
 
 @dp.message_handler(Command('chef_help'), state='*', is_chef=True)
 async def chef_help_command(message: Message) -> Message:
-    return await message.reply('Вітаємо! Щоб почати користуватись ботом, ви можете:\n'
+    return await message.reply('Вітаємо! Ви повинні керувати готовністю замовлень у вашому ресторані.\n\n'
+                               'Щоб почати користуватись ботом, ви можете:\n'
                                '* /orders - показати усі замовлення вашого ресторану\n')
 
 
 @dp.message_handler(Command('admin_help'), state='*', is_admin=True)
 async def admin_help_command(message: Message) -> Message:
-    return await message.reply('Вітаємо! Щоб почати користуватись ботом, ви можете:\n'
+    return await message.reply('Вітаємо! Ви повинні керувати замовленнями, '
+                               'місцями у ресторані та виконувати адміністративну роботу.\n\n'
+                               'Щоб почати користуватись ботом, ви можете:\n'
                                '* /orders - показати усі замовлення вашого ресторану\n'
                                '* /rs - видалити ресторан\n'
                                '* /add_rs - додати ресторан\n'
@@ -38,4 +41,5 @@ async def help_command(message: Message) -> Message:
                                '* /my_ref - створити реферальне посилання\n'
                                '* /admin_help - список команд для адміністратора\n'
                                '* /chef_help - список команд для шеф-кухара\n'
-                               '* /courier_help - список команд для кур\'єра\n')
+                               '* /courier_help - список команд для кур\'єра\n\n'
+                               'Якщо вас цікавить робота в ресторані, зверніться до @sylvenis.')
