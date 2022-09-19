03ae3d38031e53f7328fc0abe48e579707dd8802
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-09-19 13:29:30 +0300

    Add some plots

--- tgbot/misc/stats.py
+++ tgbot/misc/stats.py
@@ -9,11 +9,22 @@ matplotlib.use('TkAgg')
 def make_analysis(data: list):
     df: pd.DataFrame = prepare_df(data)
     df = casting_types(df)
-    print(df.groupby('name')['price'].agg(['count', 'mean']))
     sns.countplot(x=df['restaurant'], palette='Greens')
     plt.savefig('plot.png')
     sns.barplot(x='price', y='restaurant', hue='name', data=df, palette="Greens")
     plt.show()
+    sns.histplot(df['name'])
+    plt.show()
+    df.plot.scatter(x='time', y='restaurant')
+    plt.show()
+    d = df.groupby('name')['price'].agg(['median', 'mean'])
+    indexes = d.index
+    columns = d.columns
+    for index in indexes:
+        for column in columns:
+            print(index, column)
+            print(d.loc[index, column])
+        print()
     df.to_csv('./data.csv')
 
 
@@ -31,5 +42,5 @@ def prepare_df(data: list):
 
 
 def casting_types(df: pd.DataFrame):
-    df['time'] = pd.to_datetime(df['time'])
+    df['time'] = pd.to_datetime(df['time']).dt.normalize()
     return df
