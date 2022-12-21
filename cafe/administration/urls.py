24c51059792bc978c85f9d017571231d78b579c2
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-21 10:29:12 +0200

    Modify with black

--- cafe/administration/urls.py
+++ cafe/administration/urls.py
@@ -5,21 +5,29 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 router = SimpleRouter(trailing_slash=False)  # trailing slash is False for production
-router.register(r'places', views.PlaceViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'items', views.ItemViewSet)
-router.register(r'restaurants', views.RestaurantViewSet)
-router.register(r'referrals', views.ReferralViewSet)
-router.register(r'users', views.UserViewSet)
-router.register(r'orders', views.OrderViewSet)
-router.register(r'order_items', views.OrderItemViewSet)
-router.register(r'subcategories', views.SubCategoryViewSet)
+router.register(r"places", views.PlaceViewSet)
+router.register(r"categories", views.CategoryViewSet)
+router.register(r"items", views.ItemViewSet)
+router.register(r"restaurants", views.RestaurantViewSet)
+router.register(r"referrals", views.ReferralViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"orders", views.OrderViewSet)
+router.register(r"order_items", views.OrderItemViewSet)
+router.register(r"subcategories", views.SubCategoryViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('places/restaurant/<str:restaurant_name>', views.places_by_restaurant, name='places_by_restaurant'),
-    path('items/by/<str:subcategory_code>', views.get_items, name='items_specific'),
-    path('subcategories/by/<str:category_code>', views.subcategories_by_category, name='get_subcategories'),
-    path('orders/by/<str:username>', views.get_orders, name='get_orders'),
-    path('token', obtain_auth_token, name='token'),
+    path("", include(router.urls)),
+    path(
+        "places/restaurant/<str:restaurant_name>",
+        views.places_by_restaurant,
+        name="places_by_restaurant",
+    ),
+    path("items/by/<str:subcategory_code>", views.get_items, name="items_specific"),
+    path(
+        "subcategories/by/<str:category_code>",
+        views.subcategories_by_category,
+        name="get_subcategories",
+    ),
+    path("orders/by/<str:username>", views.get_orders, name="get_orders"),
+    path("token", obtain_auth_token, name="token"),
 ]
