ad30cbb9479bb6c01e6938810ea1f9a425845249
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-24 15:59:06 +0200

    Some updates

--- tgbot/handlers/callbacks.py
+++ tgbot/handlers/callbacks.py
@@ -12,74 +12,74 @@ from tgbot.misc.backend import User, Referral
 from tgbot.misc.invoices.invoice import Item
 
 
-@dp.callback_query_handler(text='busy_place')
+@dp.callback_query_handler(text="busy_place")
 async def place_callback(call: CallbackQuery) -> Message:
-    return await call.message.answer('Це місце зайнято. Оберіть інше.')
+    return await call.message.answer("Це місце зайнято. Оберіть інше.")
 
 
-@dp.callback_query_handler(text_contains='rs:')
+@dp.callback_query_handler(text_contains="rs:")
 async def place_callback(call: CallbackQuery) -> Message:
-    restaurant_id = call.data.split(':')[1]
-    api = call.bot.get('restaurants_api')
+    restaurant_id = call.data.split(":")[1]
+    api = call.bot.get("restaurants_api")
     status = await api.delete_restaurant(restaurant_id)
     if status == 204:
         restaurants = await api.get_all_restaurants()
         keyboard = await restaurants_markup(restaurants)
-        text = f'Список ресторанів у базі даних. Щоб видалити, {hbold("натисніть хрестик")} навпроти імені номеру' \
-               f' ресторану:\n'
+        text = (
+            f'Список ресторанів у базі даних. Щоб видалити, {hbold("натисніть хрестик")} навпроти імені номеру'
+            f" ресторану:\n"
+        )
         for index, restaurant in enumerate(restaurants, start=1):
             text += f'{hitalic(index)}. {restaurant["name"]}\n'
         await call.message.edit_text(text, reply_markup=keyboard)
-        return await call.message.answer('Ресторан було видалено з бази даних.')
-    return await call.message.answer('Помилка при видаленні ресторану.')
+        return await call.message.answer("Ресторан було видалено з бази даних.")
+    return await call.message.answer("Помилка при видаленні ресторану.")
 
 
-@dp.callback_query_handler(place_data.filter(choice='yes'))
+@dp.callback_query_handler(place_data.filter(choice="yes"))
 async def accept_offer(call: CallbackQuery, callback_data: dict) -> Message:
-    number = callback_data['number']
-    restaurant = callback_data['restaurant']
-    api: User = call.bot.get('users_api')
+    number = callback_data["number"]
+    restaurant = callback_data["restaurant"]
+    api: User = call.bot.get("users_api")
     user = await api.get_user(call.from_user.username)
     need_email = False
-    prices = [
-        LabeledPrice(
-            label='Оренда столика',
-            amount=100 * 100
-        )
-    ]
-    if 'detail' in user.keys():
+    prices = [LabeledPrice(label="Оренда столика", amount=100 * 100)]
+    if "detail" in user.keys():
         need_email = True
     else:
-        ref_api: Referral = call.bot.get('referrals_api')
+        ref_api: Referral = call.bot.get("referrals_api")
         if discount := await ref_api.get_discount(call.from_user.username):
-            prices.append(LabeledPrice(label='Знижка',
-                          amount=int(-100 * discount)))
+            prices.append(LabeledPrice(label="Знижка", amount=int(-100 * discount)))
     place_invoice = Item(
-        title=f'Місце з номером {number}',
-        description=f'Оренда місця з номером {number} у ресторані {restaurant} '
-                    f'на дату {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+        title=f"Місце з номером {number}",
+        description=f"Оренда місця з номером {number} у ресторані {restaurant} "
+        f'на дату {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
         prices=prices,
-        start_parameter=f'create_invoice_rent_place_{number}',
-        payload=f'place:{number}:{restaurant}',
-        need_email=need_email
+        start_parameter=f"create_invoice_rent_place_{number}",
+        payload=f"place:{number}:{restaurant}",
+        need_email=need_email,
     )
     return await bot.send_invoice(call.from_user.id, **place_invoice.generate_invoice())
 
 
-@dp.callback_query_handler(place_data.filter(choice='no'))
+@dp.callback_query_handler(place_data.filter(choice="no"))
 async def deny_offer(call: CallbackQuery, callback_data: dict) -> Message:
-    markup = await places_markup(call.message, callback_data['restaurant'])
+    markup = await places_markup(call.message, callback_data["restaurant"])
     message = call.message
-    await message.answer('Оберіть вільне місце, яке хочете забронювати', reply_markup=markup)
-    return await message.reply('Оренда буде дійсна дві години від початку оренди')
+    await message.answer(
+        "Оберіть вільне місце, яке хочете забронювати", reply_markup=markup
+    )
+    return await message.reply("Оренда буде дійсна дві години від початку оренди")
 
 
-@dp.callback_query_handler(text_contains='place')
+@dp.callback_query_handler(text_contains="place")
 async def place_callback(call: CallbackQuery) -> Message:
     place = call.data.split(":")[-2]
-    restaurant_name = call.data.split(':')[-1]
+    restaurant_name = call.data.split(":")[-1]
     markup = place_markup(place, restaurant_name)
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    text = f'Ви впевнені у оренді місця з номером {hbold(place)}?\nОренда буде записана на ім\'я: ' \
-           f'{hbold(call.from_user.full_name)}\nПочаток оренди: {hbold(date)}'
+    text = (
+        f"Ви впевнені у оренді місця з номером {hbold(place)}?\nОренда буде записана на ім'я: "
+        f"{hbold(call.from_user.full_name)}\nПочаток оренди: {hbold(date)}"
+    )
     return await call.message.answer(text, reply_markup=markup)
--- tgbot/handlers/menu.py
+++ tgbot/handlers/menu.py
@@ -10,7 +10,7 @@ from tgbot.keyboards.inline.menu_keyboards import (
     categories_keyboard,
     subcategories_keyboard,
     items_keyboard,
-    item_keyboard
+    item_keyboard,
 )
 
 
@@ -20,7 +20,7 @@ async def show_menu(message: Message):
 
 
 async def list_categories(message: Union[Message, CallbackQuery], **kwargs):
-    markup = await categories_keyboard(message.bot.get('items_api'))
+    markup = await categories_keyboard(message.bot.get("items_api"))
     if isinstance(message, Message):
         await message.answer("Дивись, що у нас є", reply_markup=markup)
     elif isinstance(message, CallbackQuery):
@@ -29,17 +29,21 @@ async def list_categories(message: Union[Message, CallbackQuery], **kwargs):
 
 
 async def list_subcategories(callback: CallbackQuery, category: str, **kwargs):
-    markup = await subcategories_keyboard(callback.bot.get('items_api'), category)
+    markup = await subcategories_keyboard(callback.bot.get("items_api"), category)
     await callback.message.edit_reply_markup(markup)
 
 
-async def list_items(callback: CallbackQuery, category: str, subcategory: str, **kwargs):
-    markup = await items_keyboard(callback.bot.get('items_api'), category, subcategory)
+async def list_items(
+    callback: CallbackQuery, category: str, subcategory: str, **kwargs
+):
+    markup = await items_keyboard(callback.bot.get("items_api"), category, subcategory)
     await callback.message.edit_text(text="Дивись, що у нас є", reply_markup=markup)
 
 
-async def show_item(callback: CallbackQuery, category: str, subcategory: str, item_id: str, **kwargs):
-    api = callback.bot.get('items_api')
+async def show_item(
+    callback: CallbackQuery, category: str, subcategory: str, item_id: str, **kwargs
+):
+    api = callback.bot.get("items_api")
     markup = await item_keyboard(api, category, subcategory, item_id)
     item = await api.get_item(item_id)
     text = f'Купити {item["name"]} ({item["price"]} гривень.)'
@@ -48,24 +52,26 @@ async def show_item(callback: CallbackQuery, category: str, subcategory: str, it
 
 async def buy_item(callback: CallbackQuery, item_id: str, state: FSMContext, **kwargs):
     await state.update_data(item_id=item_id)
-    await state.set_state('item_amount')
-    return await callback.message.answer('Введіть кількість товару(у цифрах):')
+    await state.set_state("item_amount")
+    return await callback.message.answer("Введіть кількість товару(у цифрах):")
 
 
 @dp.callback_query_handler(menu_callback.filter())
 async def menu_navigate(call: CallbackQuery, callback_data: dict, state: FSMContext):
-    current_level = callback_data.get('level')
-    category = callback_data.get('category')
-    subcategory = callback_data.get('subcategory')
-    item_id = callback_data.get('item_id')
+    current_level = callback_data.get("level")
+    category = callback_data.get("category")
+    subcategory = callback_data.get("subcategory")
+    item_id = callback_data.get("item_id")
 
     levels = {
-        '0': list_categories,
-        '1': list_subcategories,
-        '2': list_items,
-        '3': show_item,
-        '4': buy_item
+        "0": list_categories,
+        "1": list_subcategories,
+        "2": list_items,
+        "3": show_item,
+        "4": buy_item,
     }
     current_level_function = levels.get(current_level)
 
-    await current_level_function(call, category=category, subcategory=subcategory, item_id=item_id, state=state)
+    await current_level_function(
+        call, category=category, subcategory=subcategory, item_id=item_id, state=state
+    )
