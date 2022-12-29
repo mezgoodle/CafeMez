bd646b43defa595344353287157b7385a8dcb05f
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-12-29 12:48:45 +0200

    Some updates

--- cafe/administration/serializers.py
+++ cafe/administration/serializers.py
@@ -1,24 +1,34 @@
 from rest_framework import serializers
 
-from .models import Place, Restaurant, User, Referral, Item, Category, SubCategory, Order, OrderItem
+from .models import (
+    Place,
+    Restaurant,
+    User,
+    Referral,
+    Item,
+    Category,
+    SubCategory,
+    Order,
+    OrderItem,
+)
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = '__all__'
-        lookup_field = 'name'
+        fields = "__all__"
+        lookup_field = "name"
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-        lookup_field = 'code'
+        fields = "__all__"
+        lookup_field = "code"
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['items_amount'] = instance.count_items()
+        data["items_amount"] = instance.count_items()
         return data
 
 
@@ -27,11 +37,11 @@ class SubCategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubCategory
-        fields = '__all__'
+        fields = "__all__"
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['items_amount'] = instance.count_items()
+        data["items_amount"] = instance.count_items()
         return data
 
 
@@ -40,35 +50,46 @@ class ItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = "__all__"
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        fields = "__all__"
         depth = 1
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ReferralSerializer(serializers.ModelSerializer):
     class Meta:
         model = Referral
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'is_staff', 'is_superuser', 'password', 'telegram_id', 'is_chef',
-                  'is_courier', 'connected_restaurant', 'referred']
-        extra_kwargs = {'password': {'write_only': True}}
-        lookup_field = 'username'
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_staff",
+            "is_superuser",
+            "password",
+            "telegram_id",
+            "is_chef",
+            "is_courier",
+            "connected_restaurant",
+            "referred",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+        lookup_field = "username"
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -80,11 +101,11 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = "__all__"
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data['items'] = OrderItemSerializer(instance.get_items(), many=True).data
+        data["items"] = OrderItemSerializer(instance.get_items(), many=True).data
         return data
 
     def get_total_price(self, instance):
