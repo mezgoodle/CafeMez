c5f6ceec67b3c1762b2b45d9888a0c747f04aaf4
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-02-02 12:52:01 +0200

    Write some docstring

--- tgbot/misc/admin_utils.py
+++ tgbot/misc/admin_utils.py
@@ -1,24 +1,44 @@
+from typing import Tuple
+
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold, hitalic
 
 from tgbot.states.states import User
 
-from typing import Tuple
-
 
 def check_username(command: Command.CommandObj) -> Tuple[bool, str]:
+    """Function for checking right writing of nickname
+
+    Args:
+        command (Command.CommandObj): telegram message with nickname object
+
+    Returns:
+        Tuple[bool, str]: boolean value, if nickname was written right, and reason or username itself
+    """
     try:
-        username: str = command.args.split(' ')[0]
+        username: str = command.args.split(" ")[0]
     except (TypeError, AttributeError):
-        return False, f'Потрібно вказати {hitalic("nickname")} користувача. Приклад: /add_admin @username'
-    if not username.startswith('@'):
+        return (
+            False,
+            f'Потрібно вказати {hitalic("nickname")} користувача. Приклад: /add_admin @username',
+        )
+    if not username.startswith("@"):
         return False, f'Нікнейм повинен писатись як {hbold("@" + username)}'
     return True, username[1:]
 
 
 async def start_registration(message: Message) -> Message:
+    """Just common logic for registration
+
+    Args:
+        message (Message): message from the telegram
+
+    Returns:
+        Message: message from the bot
+    """
     await User.first()
     return await message.answer(
-        'Перешліть будь ласка повідомлення від користувача, якого ви хочете додати в базу даних.\n'
-        f'{hbold("Уважно перегляньте налаштування приватності!")}')
+        "Перешліть будь ласка повідомлення від користувача, якого ви хочете додати в базу даних.\n"
+        f'{hbold("Уважно перегляньте налаштування приватності!")}'
+    )
--- tgbot/misc/api.py
+++ tgbot/misc/api.py
@@ -1,35 +1,76 @@
+from typing import Tuple, Union
+
 import aiohttp
 
 from tgbot.misc.decorators import api_decorator
-
-from typing import Tuple, Union
 
 
 class API:
     def __init__(self):
         self.session = aiohttp.ClientSession()
-        self.base_url = 'http://127.0.0.1:8000/api/%s'
+        self.base_url = "http://127.0.0.1:8000/api/%s"
 
     @api_decorator
     async def get(self, request_url: str) -> Union[dict, list]:
+        """Method for HTTP-get method
+
+        Args:
+            request_url (str): part of the url
+
+        Returns:
+            Union[dict, list]: dict of the object or list of the objects
+        """
         url = self.base_url % request_url
         async with self.session.get(url) as response:
             return await response.json()
 
     @api_decorator
-    async def post(self, request_url, data: dict = None, headers: dict = None) -> Tuple[dict, int]:
+    async def post(
+        self, request_url, data: dict = None, headers: dict = None
+    ) -> Tuple[dict, int]:
+        """Method for HTTP-post method
+
+        Args:
+            request_url (_type_): part of the url
+            data (dict, optional): data of the new object. Defaults to None.
+            headers (dict, optional): some additional headers. Defaults to None.
+
+        Returns:
+            Tuple[dict, int]: created object and status
+        """
         url = self.base_url % request_url
         async with self.session.post(url, data=data, headers=headers) as response:
             return await response.json(), response.status
 
     @api_decorator
-    async def put(self, request_url, data=None, headers: dict = None) -> Tuple[dict, int]:
+    async def put(
+        self, request_url, data=None, headers: dict = None
+    ) -> Tuple[dict, int]:
+        """Method for HTTP-put method
+
+        Args:
+            request_url (_type_): part of the url
+            data (_type_, optional): new data. Defaults to None.
+            headers (dict, optional): some additional headers. Defaults to None.
+
+        Returns:
+            Tuple[dict, int]: updated object and status
+        """
         url = self.base_url % request_url
         async with self.session.patch(url, data=data, headers=headers) as response:
             return await response.json(), response.status
 
     @api_decorator
     async def delete(self, request_url: str, headers: dict = None) -> int:
+        """Method for HTTP-delete method
+
+        Args:
+            request_url (str): part of the url
+            headers (dict, optional): some additional headers. Defaults to None.
+
+        Returns:
+            int: status
+        """
         url = self.base_url % request_url
         async with self.session.delete(url, headers=headers) as response:
             return response.status
