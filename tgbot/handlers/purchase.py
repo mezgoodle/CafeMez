b49ba5ad806c9f5a2689b27687ec390f20f2d347
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-02-06 15:43:14 +0200

    Add shipping price limitation

--- tgbot/handlers/purchase.py
+++ tgbot/handlers/purchase.py
@@ -2,173 +2,208 @@ from datetime import datetime
 
 from aiogram import Bot
 from aiogram.dispatcher import FSMContext
-from aiogram.utils.markdown import hbold
 from aiogram.dispatcher.filters import Text
-from aiogram.types import CallbackQuery, Message, ContentType, LabeledPrice
+from aiogram.types import CallbackQuery, ContentType, LabeledPrice, Message
+from aiogram.utils.markdown import hbold
 
-from tgbot.handlers.menu import list_categories
 from loader import dp
-from tgbot.keyboards.inline.cart_keyboard import cart_keyboard
+from tgbot.handlers.menu import list_categories
 from tgbot.keyboards.inline.callback_data import cart_callback
-from tgbot.keyboards.reply.restaurants import restaurants_markup
+from tgbot.keyboards.inline.cart_keyboard import cart_keyboard
 from tgbot.keyboards.reply.payment import payments_markup
-from tgbot.misc.backend import Item, Restaurant, Order, User, Referral
-from tgbot.misc.storage import Storage
+from tgbot.keyboards.reply.restaurants import restaurants_markup
+from tgbot.misc.backend import Item, Order, Referral, Restaurant, User
 from tgbot.misc.invoices.invoice import Item as ItemInvoice
+from tgbot.misc.storage import Storage
 
 
-@dp.message_handler(lambda message: not message.text.isdigit() and int(message.text) <= 0,
-                    state=['item_amount', 'change_amount'])
+@dp.message_handler(
+    lambda message: not message.text.isdigit() and int(message.text) <= 0,
+    state=["item_amount", "change_amount"],
+)
 async def item_amount_handler(message: Message):
-    return await message.reply(f'Введіть кількість товару у {hbold("цифрах")} та більше нуля')
+    return await message.reply(
+        f'Введіть кількість товару у {hbold("цифрах")} та більше нуля'
+    )
 
 
-@dp.message_handler(state='item_amount')
+@dp.message_handler(state="item_amount")
 async def item_amount(message: Message, state: FSMContext):
     data = await state.get_data()
-    item_id = data.get('item_id')
+    item_id = data.get("item_id")
     amount = int(message.text)
-    storage: Storage = message.bot.get('storage')
+    storage: Storage = message.bot.get("storage")
     storage.add_to_cart(message.from_user.id, item_id, amount)
     await state.reset_state()
-    await message.answer('Ви успішно додали товар до корзини! Щоб її переглянути, натисніть /cart')
+    await message.answer(
+        "Ви успішно додали товар до корзини! Щоб її переглянути, натисніть /cart"
+    )
     return await list_categories(message)
 
 
-@dp.message_handler(state='change_amount')
+@dp.message_handler(state="change_amount")
 async def change_item_amount(message: Message, state: FSMContext):
     data = await state.get_data()
-    item_id = data.get('item_id')
+    item_id = data.get("item_id")
     amount = int(message.text)
-    storage: Storage = message.bot.get('storage')
+    storage: Storage = message.bot.get("storage")
     storage.change_amount(message.from_user.id, item_id, amount)
     cart = storage.get_cart(message.from_user.id)
     await state.reset_state()
-    await message.answer('Ви успішно змінили кількість товару!')
-    api: Item = message.bot.get('items_api')
+    await message.answer("Ви успішно змінили кількість товару!")
+    api: Item = message.bot.get("items_api")
     keyboard = await cart_keyboard(api, cart)
-    return await message.answer('Ваша оновлена корзина:', reply_markup=keyboard)
+    return await message.answer("Ваша оновлена корзина:", reply_markup=keyboard)
 
 
-@dp.callback_query_handler(cart_callback.filter(action='show'))
+@dp.callback_query_handler(cart_callback.filter(action="show"))
 async def show_item(callback_query: CallbackQuery, callback_data: dict):
-    item_id = callback_data['item_id']
-    api: Item = callback_query.bot.get('items_api')
+    item_id = callback_data["item_id"]
+    api: Item = callback_query.bot.get("items_api")
     item = await api.get_item(item_id)
-    return await callback_query.message.answer_photo(item['photo'], f'{item["name"]} - {item["price"]} грн')
+    return await callback_query.message.answer_photo(
+        item["photo"], f'{item["name"]} - {item["price"]} грн'
+    )
 
 
-@dp.callback_query_handler(cart_callback.filter(action='change'))
-async def change_amount(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
-    item_id = callback_data['item_id']
+@dp.callback_query_handler(cart_callback.filter(action="change"))
+async def change_amount(
+    callback_query: CallbackQuery, callback_data: dict, state: FSMContext
+):
+    item_id = callback_data["item_id"]
     await state.update_data(item_id=item_id)
-    await state.set_state('change_amount')
-    return await callback_query.message.answer('Введіть нову кількість товару')
+    await state.set_state("change_amount")
+    return await callback_query.message.answer("Введіть нову кількість товару")
 
 
-@dp.callback_query_handler(cart_callback.filter(action='remove'))
+@dp.callback_query_handler(cart_callback.filter(action="remove"))
 async def delete_item(callback_query: CallbackQuery, callback_data: dict):
-    item_id = callback_data['item_id']
-    storage: Storage = callback_query.bot.get('storage')
+    item_id = callback_data["item_id"]
+    storage: Storage = callback_query.bot.get("storage")
     storage.remove_from_cart(callback_query.from_user.id, item_id)
     cart = storage.get_cart(callback_query.from_user.id)
-    await callback_query.message.answer('Ви видалили товар з корзини!')
+    await callback_query.message.answer("Ви видалили товар з корзини!")
     if cart:
-        api: Item = callback_query.bot.get('items_api')
+        api: Item = callback_query.bot.get("items_api")
         keyboard = await cart_keyboard(api, cart)
-        return await callback_query.message.answer('Ваша оновлена корзина:', reply_markup=keyboard)
+        return await callback_query.message.answer(
+            "Ваша оновлена корзина:", reply_markup=keyboard
+        )
 
 
-@dp.callback_query_handler(text_contains='cancel_cart')
+@dp.callback_query_handler(text_contains="cancel_cart")
 async def cancel_purchase(callback_query: CallbackQuery):
-    storage: Storage = callback_query.bot.get('storage')
+    storage: Storage = callback_query.bot.get("storage")
     storage.clean_cart(callback_query.from_user.id)
-    await callback_query.message.answer('Ви відмінили покупку!')
+    await callback_query.message.answer("Ви відмінили покупку!")
     return await list_categories(callback_query.message)
 
 
-@dp.callback_query_handler(text_contains='buy:')
+@dp.callback_query_handler(text_contains="buy:")
 async def apply_purchase(callback_query: CallbackQuery, state: FSMContext):
-    await state.set_state('shipping_address')
+    await state.set_state("shipping_address")
     keyboard = await restaurants_markup(callback_query.message)
-    text = 'Чи бажаєте ви доставку замовлення?' \
-           f'\nЯкщо {hbold("так")}, надішліть координати доставки.' \
-           f'\nЯкщо {hbold("ні")}, виберіть один із ресторанів'
+    text = (
+        "Чи бажаєте ви доставку замовлення?"
+        f'\nЯкщо {hbold("так")}, надішліть координати доставки.'
+        f'\nЯкщо {hbold("ні")}, виберіть один із ресторанів'
+    )
     return await callback_query.message.answer(text, reply_markup=keyboard)
 
 
-@dp.message_handler(state='shipping_address', content_types=ContentType.TEXT)
+@dp.message_handler(state="shipping_address", content_types=ContentType.TEXT)
 async def shipping_address_as_text(message: Message, state: FSMContext):
     restaurant_name = message.text
-    api: Restaurant = message.bot.get('restaurants_api')
+    api: Restaurant = message.bot.get("restaurants_api")
     restaurant = await api.get_restaurant(restaurant_name)
-    if name := restaurant['name']:
+    if name := restaurant["name"]:
         await state.update_data(shipping_address_name=name)
-        await state.set_state('payment_method')
+        await state.set_state("payment_method")
         keyboard = payments_markup()
-        return await message.answer('Виберіть спосіб оплати', reply_markup=keyboard)
+        return await message.answer("Виберіть спосіб оплати", reply_markup=keyboard)
 
 
-@dp.message_handler(state='shipping_address',
-                    content_types=ContentType.LOCATION)
+@dp.message_handler(state="shipping_address", content_types=ContentType.LOCATION)
 async def shipping_address_as_location(message: Message, state: FSMContext):
     restaurant_location = message.location
-    api: Order = message.bot.get('orders_api')
-    restaurant_name, shipping_price = await api.get_shipping_price(restaurant_location, message.bot)
-    await state.update_data(shipping_price=round(shipping_price), shipping_address_name=restaurant_name,
-                            shipping_address_longitude=float(
-                                restaurant_location.longitude),
-                            shipping_address_latitude=float(restaurant_location.latitude))
-    await state.set_state('payment_method')
+    api: Order = message.bot.get("orders_api")
+    restaurant_name, shipping_price = await api.get_shipping_price(
+        restaurant_location, message.bot
+    )
+    if shipping_price > 1000:
+        return await message.answer(
+            "Ви знаходитесь поза зоною доставки. Оберіть ближче місце або самовивіз з ресторану."
+        )
+    await state.update_data(
+        shipping_price=round(shipping_price),
+        shipping_address_name=restaurant_name,
+        shipping_address_longitude=float(restaurant_location.longitude),
+        shipping_address_latitude=float(restaurant_location.latitude),
+    )
+    await state.set_state("payment_method")
     keyboard = payments_markup()
-    return await message.answer('Виберіть спосіб оплати', reply_markup=keyboard)
+    return await message.answer("Виберіть спосіб оплати", reply_markup=keyboard)
 
 
-@dp.message_handler(Text(equals=['Картка', 'Готівка'],
-                    ignore_case=True), state='payment_method')
+@dp.message_handler(
+    Text(equals=["Картка", "Готівка"], ignore_case=True), state="payment_method"
+)
 async def answer_payment_method(message: Message, state: FSMContext):
-    payment_dict = {'Картка': 'CD', 'Готівка': 'CH'}
+    payment_dict = {"Картка": "CD", "Готівка": "CH"}
     payment_method = payment_dict[message.text]
     await state.update_data(payment_method=payment_method)
     data = await state.get_data()
     bot: Bot = message.bot
     await state.finish()
-    api: Order = bot.get('orders_api')
+    api: Order = bot.get("orders_api")
     order_id, status = await api.create_order(user=message.from_user.username, **data)
     if status == 201:
-        storage: Storage = bot.get('storage')
+        storage: Storage = bot.get("storage")
         cart = storage.get_cart(message.from_user.id)
         status, prices = await api.add_order_items(order_id, cart)
         if status == 201:
             storage.clean_cart(message.from_user.id)
-            if payment_method == 'CD':
-                users_api: User = bot.get('users_api')
+            if payment_method == "CD":
+                users_api: User = bot.get("users_api")
                 order = await api.get_order(str(order_id))
-                if price := order['shipping_price']:
+                if price := order["shipping_price"]:
                     prices.append(
-                        LabeledPrice(
-                            label='Вартість доставки',
-                            amount=int(price) * 100))
+                        LabeledPrice(label="Вартість доставки", amount=int(price) * 100)
+                    )
                 user = await users_api.get_user(message.from_user.username)
                 need_email = False
-                if 'detail' in user.keys():
+                if "detail" in user.keys():
                     need_email = True
                 else:
-                    ref_api: Referral = message.bot.get('referrals_api')
-                    if discount := await ref_api.get_discount(message.from_user.username):
+                    ref_api: Referral = message.bot.get("referrals_api")
+                    if discount := await ref_api.get_discount(
+                        message.from_user.username
+                    ):
                         prices.append(
-                            LabeledPrice(label='Знижка', amount=int(-order['total_price_without_discount'] * discount)))
+                            LabeledPrice(
+                                label="Знижка",
+                                amount=int(
+                                    -order["total_price_without_discount"] * discount
+                                ),
+                            )
+                        )
                 order_invoice = ItemInvoice(
-                    title=f'Замовлення з номером {order_id}',
+                    title=f"Замовлення з номером {order_id}",
                     description=f'Замовлення номер {order_id} у ресторані {order["shipping_address_name"]["name"]}'
-                                f'{" із доставкою" if order["shipping_address_longitude"] else ""}. '
-                                f'Створене {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+                    f'{" із доставкою" if order["shipping_address_longitude"] else ""}. '
+                    f'Створене {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
                     prices=prices,
-                    start_parameter=f'create_invoice_order_{order_id}',
+                    start_parameter=f"create_invoice_order_{order_id}",
                     payload=f'order:{order_id}:{order["shipping_address_name"]["name"]}',
-                    need_email=need_email
+                    need_email=need_email,
                 )
-                await message.answer('Ваше замовлення прийнято! Оплатіть його будь ласка.')
-                return await bot.send_invoice(message.from_user.id, **order_invoice.generate_invoice())
-            return await message.answer('Ваше замовлення прийнято!. Щоб побачити свої замовлення, натисніть /my_orders')
-    return await message.answer('Виникла помилка при оформленні замовлення!')
+                await message.answer(
+                    "Ваше замовлення прийнято! Оплатіть його будь ласка."
+                )
+                return await bot.send_invoice(
+                    message.from_user.id, **order_invoice.generate_invoice()
+                )
+            return await message.answer(
+                "Ваше замовлення прийнято!. Щоб побачити свої замовлення, натисніть /my_orders"
+            )
+    return await message.answer("Виникла помилка при оформленні замовлення!")
